473e5ff6843c305d8a3ac77340571d3f038998a3
Michael Ong <michael.ong@constructor.io>
2023-11-04 02:03:22 +0800

    init rubik cube timer

---
+++ src/pages/rubik-cube-timer/index.tsx
@@ -0,0 +1,113 @@
+import { Box, Flex, Text } from "@chakra-ui/react"
+import { useState } from "react"
+
+const SECONDS_BEFORE_STARTING = 10 // in microseconds
+
+const formatStopwatch = (time: number) => {
+  const minute: number = Math.floor(time / 6000)
+  let minuteValue = minute.toString()
+  const second = time % 6000
+  let secondValue = second.toString()
+
+  if (minute >= 0 && minute < 10) {
+    minuteValue = `0${minute}`
+  }
+
+  if (second >= 0 && second < 10) {
+    secondValue = `000${second}`
+  } else if (second >= 10 && second < 100) {
+    secondValue = `00${second}`
+  } else if (second >= 100 && second < 1000) {
+    secondValue = `0${second}`
+  } else {
+    secondValue = `${second}`
+  }
+
+  return `${minuteValue}:${secondValue}`
+}
+
+const getBgColor = (isTimerRunning: boolean, falseStartTimer: number) => {
+  if (isTimerRunning || falseStartTimer >= SECONDS_BEFORE_STARTING) {
+    return "green.300"
+  }
+
+  if (falseStartTimer > 0 && falseStartTimer < SECONDS_BEFORE_STARTING) {
+    return "red.500"
+  }
+  return "white"
+}
+
+function RubikCubeTimer() {
+  const [falseStartTimer, setFalseStartTimer] = useState(0)
+  const [falseStartIntervalId, setFalseStartIntervalId] = useState<NodeJS.Timeout>()
+
+  const [currentTimer, setCurrentTimer] = useState(0)
+  const [intervalID, setIntervalID] = useState<NodeJS.Timeout>()
+  const [isTimerRunning, setIsTimerRunning] = useState(false)
+
+  const timer = (shouldStart: boolean) => {
+    if (shouldStart) {
+      const timer: ReturnType<typeof setTimeout> = setInterval(() => {
+        setCurrentTimer((prevState) => prevState + 1)
+      }, 41)
+      setIntervalID(timer)
+      setIsTimerRunning(true)
+    } else {
+      clearInterval(intervalID)
+      setIsTimerRunning(false)
+    }
+  }
+
+  /**
+   * if timer is running, on hold would reset the timer
+   * if timer is not running, a false start timer would start.
+   * A minimum of 1 sec should pass before we start a timer.
+   */
+  const onHold = () => {
+    const falseStartTimer: ReturnType<typeof setTimeout> = setInterval(() => {
+      setFalseStartTimer((prevState) => prevState + 1)
+    }, 100)
+    setFalseStartIntervalId(falseStartTimer)
+
+    if (!isTimerRunning) {
+      setCurrentTimer(0)
+    }
+  }
+
+  /**
+   * if timer is running; stop the timer
+   * if timer is not running, but false start timer > 1sec; the timer would start
+   * if timer is not running, but false start timer < 1sec; the timer would not start.
+   */
+  const onUp = () => {
+    if (isTimerRunning) {
+      timer(false)
+      setFalseStartTimer(0)
+    } else if (falseStartTimer > SECONDS_BEFORE_STARTING) {
+      timer(true)
+      setCurrentTimer(0)
+    }
+
+    clearInterval(falseStartIntervalId)
+  }
+
+  return (
+    <Box
+      w="100dvw"
+      h="100dvh"
+      onTouchStart={onHold}
+      onTouchEnd={onUp}
+      bgColor={getBgColor(isTimerRunning, falseStartTimer)}
+    >
+      <Flex justifyContent="center" alignItems="center" w="full" h="full">
+        <Flex flexDir="column" gap={4}>
+          <Text fontSize="106px" fontFamily="mono" color={isTimerRunning ? "white" : "black"}>
+            {formatStopwatch(currentTimer)}
+          </Text>
+        </Flex>
+      </Flex>
+    </Box>
+  )
+}
+
+export default RubikCubeTimer
---
+++ src/theme/typography.ts
@@ -0,0 +1,50 @@
+export const fonts = {
+  body: "Lato, sans-serif",
+  heading: "Roboto Slab Variable, sans-serif",
+  mono: "JetBrains Mono Variable,  monospace",
+  inter: "Inter, sans-serif",
+}
+
+export const fontWeights = {
+  light: 300,
+  regular: 400,
+  medium: 500,
+  semibold: 600,
+  bold: 700,
+}
+
+export const textStyles = {
+  heading1: {
+    fontFamily: "heading",
+    fontSize: "28px",
+    lineHeight: 8,
+    fontWeight: "medium",
+  },
+  heading2: {
+    fontFamily: "heading",
+    fontSize: "24px",
+    lineHeight: 7,
+    fontWeight: "medium",
+  },
+  heading3: {
+    fontFamily: "heading",
+    fontSize: "20px",
+    lineHeight: 6,
+    fontWeight: "medium",
+  },
+  body1: {
+    fontFamily: "body",
+    fontSize: "15px",
+    lineHeight: 5,
+    fontWeight: "regular",
+  },
+  body2: {
+    fontFamily: "body",
+    fontSize: "14px",
+    lineHeight: 5,
+    fontWeight: "regular",
+  },
+  code: {
+    fontFamily: "mono",
+  },
+}
